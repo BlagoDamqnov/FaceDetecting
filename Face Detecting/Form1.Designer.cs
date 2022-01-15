ceb679e79c87ae63ac3e8d7c387ea326bae1788d
BlagoDamqnov <chessplayer89@abv.bg>
2022-01-15 15:19:31 +0200

    New file

---
+++ Face Detecting/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Face_Detecting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(1093, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DETECT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.Color.White;
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic.Location = new System.Drawing.Point(7, 3);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(1080, 854);
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1343, 854);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Face Detecting";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ Face Detecting/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Face_Detecting
+{
+    public partial class Form1 : Form
+    {
+        static readonly CascadeClassifier cascadeClassfier = new CascadeClassifier("face.xml");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fd = new OpenFileDialog() {Multiselect = false,Filter = "JPEG|*.jpg"})
+            {
+                if (fd.ShowDialog()==DialogResult.OK)
+                {
+                    pic.Image = Image.FromFile(fd.FileName);
+                    Bitmap bm = new Bitmap(pic.Image);
+                    Image<Bgr,byte> grayImage = new Image<Bgr, byte>(bm);
+                    Rectangle[] rect = cascadeClassfier.DetectMultiScale(grayImage, 1.4, 0);
+                    foreach (Rectangle rectangle in rect)
+                    {
+                        using (Graphics gr = Graphics.FromImage(bm))
+                        {
+                            using(Pen pen= new Pen(Color.Red,1))
+                            {
+                               gr.DrawRectangle(pen, rectangle);
+                            }
+                        }
+                    }
+                    pic.Image = bm;
+                }
+            }
+        }
+    }
+}
